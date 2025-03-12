976d73464170bd8b65ba02c29e24afb30db3b630
wjujw123 <116214600+wjujw123@users.noreply.github.com>
2025-03-11 23:58:32 -0700

    Update Spacewar-Commander.ino
    I added some variables, but I still can't exactly visualize what they should be, so I am unsure what else to add at the moment. I would need some help with this sometime.

--- Spacewar-Commander.ino
+++ Spacewar-Commander.ino
@@ -1,8 +1,15 @@
-int knob;
-int lever;
-int reBox;
-int button;
-int LSwitch;
+int knob;//fine tuning knob?
+int lever; //
+int reBox; //reload box
+int button; //direction change button
+int sButton; //shooting button
+int LSwitch; //light sensor
+
+int knobVal; //knob's value
+boolean pressButton; //
+boolean pressSButton; //Shooting button pressed
+int LVal; // light value
+
 
 void setup() {
   // put your setup code here, to run once:
