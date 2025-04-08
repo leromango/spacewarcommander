3ce9c837935d96823752e1a8a334e080e7ffe427
leromango <soft.leromango@pm.me>
2025-04-08 11:47:51 -0700

    slider support

--- Spacewar-Commander/Spacewar-Commander.ino
+++ Spacewar-Commander/Spacewar-Commander.ino
@@ -2,38 +2,42 @@
 
 // Analog sensor inputs
 int knobPin = A0;                     // Potentiometer: resource distribution
-int flashlightSensorPin = A1;         // Light sensor 1: flashlight control
+int flashlightSensorPin = A1;         // Light sensor: flashlight control
+int sliderXPin = A2;                  // Slider X input
+int sliderYPin = A3;                  // Slider Y input
 
 // Button inputs
 int shootButtonPin = 12;             // Shoot
-int moveButtonPin = 4;              // Move 
-int changeDirectionButtonPin = 5;   // Change direction (for switch)
-int reloadPin = 6;
+int moveButtonPin = 4;               // Move 
+int changeDirectionButtonPin = 5;    // Change direction (for switch)
+int reloadPin = 6;                   // Reload
+
 // Switch
 int switchPin = 2;
 
+// Indicator lights
 int leftLightPin = 7;
 int backLightPin = 8;
 int fwdLightPin = 9;
-int rightLightPin  = 10;
+int rightLightPin = 10;
 
+// Servo and buzzer
 int servoControlPin = 11; //PWM
 Servo reloadServo;
-
 int buzzerPin = 3; //PWM
 
+// State variables
 bool isReloading = false;
-long long unsigned int reloadStartTime = 0;
-long long unsigned int reloadDuration = 500;
+unsigned long long reloadStartTime = 0;
+unsigned long long reloadDuration = 500;
 
-long long unsigned int buzzerStartTime = 0;
-long long unsigned int buzzerDuration = 10;
+unsigned long long buzzerStartTime = 0;
+unsigned long long buzzerDuration = 10;
 
-long long unsigned int indicatorDuration = 50;
-long long unsigned int indicatorStartTime = 500;
+unsigned long long indicatorDuration = 50;
+unsigned long long indicatorStartTime = 500;
 
 bool bShouldBuzz = false;
-
 bool bIsIndicatorOn = false;
 
 enum ELightIndication {
@@ -51,55 +55,58 @@ void setup() {
   currentIndicator = ELightIndication::NONE;
   Serial.begin(256000);
   Serial.setTimeout(50);
-  
+
+  // Analog inputs
   pinMode(knobPin, INPUT);
   pinMode(flashlightSensorPin, INPUT);
-  
-  // INPUT_PULLUP works better for buttons as it automatically sets 0V to 0
+  pinMode(sliderXPin, INPUT);
+  pinMode(sliderYPin, INPUT);
+
+  // Buttons and switch
   pinMode(shootButtonPin, INPUT_PULLUP);
   pinMode(moveButtonPin, INPUT_PULLUP);
   pinMode(changeDirectionButtonPin, INPUT_PULLUP);
   pinMode(reloadPin, INPUT_PULLUP);
-
   pinMode(switchPin, INPUT_PULLUP);
 
+  // Output pins
   pinMode(buzzerPin, OUTPUT);
+  pinMode(leftLightPin, OUTPUT);
+  pinMode(backLightPin, OUTPUT);
+  pinMode(fwdLightPin, OUTPUT);
+  pinMode(rightLightPin, OUTPUT);
 
   reloadServo.attach(servoControlPin);
-  reloadServo.write(50);
-
+  reloadServo.write(50); // Default position
 }
 
 void loop() {
-  long long unsigned currentMillis = millis();
+  unsigned long long currentMillis = millis();
   int reloadValue = 0;
-  
-  
-  while(Serial.available() > 0) {
+
+  while (Serial.available() > 0) {
     String incomingString = Serial.readString();
-  // Serial.println(incomingString);
+
     int buzzerIndex = incomingString.indexOf("b");
     if (buzzerIndex != -1) {
-      // buzzerDuration = (long long) incomingString.substring(buzzerIndex, incomingString.indexOf("b", 1)).toInt();
       bShouldBuzz = true;
       buzzerStartTime = currentMillis;
     }
+
     if (incomingString.indexOf("r") != -1) {
       reloadValue = 1;
       reloadStartTime = currentMillis;
     }
+
     int indexLightIndication = incomingString.indexOf("a");
     if (indexLightIndication != -1 && indicatorStartTime < currentMillis) {
-      currentIndicator = static_cast<ELightIndication>(String(incomingString.charAt(indexLightIndication+1)).toInt());
+      currentIndicator = static_cast<ELightIndication>(String(incomingString.charAt(indexLightIndication + 1)).toInt());
       indicatorStartTime = currentMillis;
       bIsIndicatorOn = true;
     }
-    
   }
-// digitalWrite(backLightPin, HIGH);
- // if (currentIndicator == 0) {
-    //digitalWrite(fwdLightPin, HIGH);
-  //}
+
+  // Handle indicator lights
   switch (currentIndicator) {
     case ELightIndication::FORWARD:
       digitalWrite(fwdLightPin, HIGH);
@@ -122,15 +129,17 @@ void loop() {
     default:
       break;
   }
+
   if (!bShouldBuzz) {
-    noTone(buzzerPin);  
+    noTone(buzzerPin);
   }
+
   if (bShouldBuzz && currentMillis - buzzerStartTime >= buzzerDuration) {
     buzzerStartTime = 0;
     bShouldBuzz = false;
     noTone(buzzerPin);
-    // tone(buzzerPin, 0);
   }
+
   if (bIsIndicatorOn && currentMillis - indicatorStartTime >= indicatorDuration) {
     indicatorStartTime = 0;
     bIsIndicatorOn = false;
@@ -140,20 +149,19 @@ void loop() {
     digitalWrite(backLightPin, LOW);
     digitalWrite(leftLightPin, LOW);
   }
-  // INPUT END
-  // OUTPUT 
-  
-   
+
+  // Read analog and digital input values
   int knobValue = analogRead(knobPin);
   int flashlightValue = analogRead(flashlightSensorPin);
-  
+  int sliderXValue = analogRead(sliderXPin);
+  int sliderYValue = analogRead(sliderYPin);
+
   int shootState = digitalRead(shootButtonPin);
   int moveState = digitalRead(moveButtonPin);
   int changeDirectionState = digitalRead(changeDirectionButtonPin);
-  //int reloadValue = digitalRead(reloadPin);
-
   int switchState = digitalRead(switchPin);
 
+  // Handle reload servo
   if (reloadValue == 1 && !isReloading) {
     isReloading = true;
     reloadStartTime = millis();
@@ -166,59 +174,21 @@ void loop() {
     reloadStartTime = 0;
   }
 
-  
+  // Buzz if needed
   if (bShouldBuzz) {
-    //buzzerStartTime++;
     tone(buzzerPin, 100);
-    // bShouldBuzz = false;
   }
-  if (bIsIndicatorOn) {
-    //indicatorStartTime++;  
-  }
-  /*
-  digitalWrite(fwdLightPin, HIGH);
-  delay(100);
-  digitalWrite(rightLightPin, HIGH);
-  delay(100);
-  digitalWrite(backLightPin, HIGH);
-  delay(100);
-  digitalWrite(leftLightPin, HIGH);
-  delay(100);
 
-  digitalWrite(fwdLightPin, LOW);
-  delay(100);
-  digitalWrite(rightLightPin, LOW);
-  delay(100);
-  digitalWrite(backLightPin, LOW);
-  delay(100);
-  digitalWrite(leftLightPin, LOW);
+  // Send sensor values over serial
+  Serial.println(String(knobValue) + "," + 
+                 String(shootState) + "," + 
+                 String(moveState) + "," + 
+                 String(switchState) + "," + 
+                 String(changeDirectionState) + "," + 
+                 String(flashlightValue) + "," + 
+                 String(reloadValue) + "," + 
+                 String(sliderXValue) + "," + 
+                 String(sliderYValue));
 
-  
-  //tone(buzzerPin, 1000); // Send 1KHz sound signal...
-  //delay(1000);
-
-  //noTone(buzzerPin);
-  */
-  /*Serial.print(knobValue);
-  Serial.print(",");
-  Serial.print(shootState);
-  Serial.print(",");
-  Serial.print(moveState);
-  Serial.print(",");
-  Serial.print(switchState);
-  Serial.print(",");
-  Serial.print(changeDirectionState);
-  Serial.print(",");
-  Serial.print(flashlightValue);
-  Serial.print(",");
-  Serial.println(reloadValue);
-  */
-
-
-
-  Serial.println(String(knobValue) + "," + String(shootState) + "," + String(moveState) + "," + String(switchState) + "," + String(changeDirectionState) + "," + String(flashlightValue) + "," + String(reloadValue));
-  
-  
-  
   delay(100);
 }
